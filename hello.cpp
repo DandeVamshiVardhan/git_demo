d81ab2966e01feb3dd1951973116c7bb9d07d4d6
DandeVamshiVardhan <vamshivamshi3452@gmail.com>
2025-05-21 17:00:25 +0530

    added simple code

--- hello.cpp
+++ hello.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 int getFact(int n){
-    if(n<=1){
-        return n;
+    int ans =1;
+    for(int i=1;i<=n;i++){
+        ans*=i;
     }
-    return n* getFact(n-1);
+    return ans;
 }
 
 int getFactorialSum(int n){

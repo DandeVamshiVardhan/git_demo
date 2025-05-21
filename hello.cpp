44869860c8c21efe3deba57e1e692d4901150f2d
harshalkotkar156 <harshalkotkar156@gmail.com>
2025-05-21 16:46:26 +0530

    added recursive function

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int getFact(int n){
+    if(n<=1){
+        return n;
+    }
+    return n* getFact(n-1);
+}
 
+int getFactorialSum(int n){
+
+    int ans =0 ;
+    for(int i=1;i<=n;i++){
+        ans += getFact(i);
+    }
+    return ans;
+    
+}
 
 int main()
 {
-    int ans = getFact(0);
+    int ans = getFactorialSum(3);
     cout<<ans<<endl;
-
+    
     return 0;
 }
